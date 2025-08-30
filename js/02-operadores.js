6017018596cfce2db6eeb8fe0f3550d13a29ff2a
Pedro Henrique <pedropjh12@gmail.com>
2025-08-29 21:55:08 -0300

    Add files via upload

---
+++ js/02-operadores.js
@@ -0,0 +1,16 @@
+let numero1 = 90
+let numero2 = 3.4
+
+let soma = numero1 + numero2
+let sub = numero1 - numero2
+console.log(soma)
+console.log(sub)
+
+const numero3 = 5;
+const numero4 = 3;
+
+let exponenciacao = numero3 ** numero4;
+console.log(exponenciacao);
+
+let modulo = numero3 % numero4;
+console.log(modulo);
